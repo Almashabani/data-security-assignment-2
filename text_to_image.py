adfb30c602bf142a5e00aeb7aa0d8632b92146aa
Almashabani <alma.shabani3@student.uni-pr.edu>
2026-04-14 20:13:35 +0200

    encryptor
    Shtimi i  leximit te key.json dhe nxjerrja e mapping dhe gjatesise se tekstit.

--- text_to_image.py
+++ text_to_image.py
@@ -16,4 +16,8 @@ import json
 
 
 def decrypt_image_to_text(input_image, input_key):
-    pass
+    with open(input_key, "r", encoding="utf-8") as f:
+        key_data = json.load(f)
+
+    text_length = key_data["text_length"]
+    mapping = key_data["mapping"]
